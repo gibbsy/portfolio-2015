323d4ff9cbca5e6899ea47024be4099879a542af
Andy Gillon <aawgillon@gmail.com>
2020-10-23 13:23:05 +0100

    changed image urls to relative paths

--- app/scripts/controllers/projectCtrl.js
+++ app/scripts/controllers/projectCtrl.js
@@ -47,7 +47,7 @@ angular.module("angularApp").controller("ProjectCtrl", [
     $scope.imagesReady = "wait";
 
     var imgUrls = [],
-      domain = "http://" + $window.location.host;
+      domain = "https://" + $window.location.host;
 
     var container = angular.element(".view-container"),
       threeView = angular.element(".three");
@@ -280,6 +280,7 @@ angular.module("angularApp").controller("ProjectCtrl", [
 
       /* $scope.target = {};*/ if ($scope.project.content_images.length) {
         for (var i = 1; i < $scope.project.content_images.length; i++) {
+          // imgUrls.push(domain + $scope.project.content_images[i]);
           imgUrls.push(domain + $scope.project.content_images[i]);
         }
       } else {
